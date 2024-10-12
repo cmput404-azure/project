7500a6bf1361c4fa24b4c69572be95fb78b47d67
xirui1219 <crystal7@ualberta.ca>
2024-10-12 15:13:14 -0600

    undoing changes in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
-import ListItem from './components/ListItem/ListItem';
 
 
 export default function App() {
   return (
     <div className={styles.App}>
       Social distribution
-      <ListItem></ListItem>
-
     </div>
   );
 }
