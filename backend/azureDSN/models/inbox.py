4d6f7bf8ae209bc245fb973ff93ce9bbbe74c44d
Quin Nguyen <hnguyen6@ualberta.ca>
2024-10-12 23:55:33 -0600

    dont use camelCase

--- backend/azureDSN/models/inbox.py
+++ backend/azureDSN/models/inbox.py
@@ -1,10 +1,10 @@
 from django.db import models
 from datetime import datetime
 from .user import User
-from .inboxItem import InboxItem
+from .inbox_item import InboxItem
 
 
 class Inbox(models.Model):
     # Each user will have her/his own inbox which basically stores all types of items: post, follow, comment and like
-    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     items = models.ManyToManyField(InboxItem)
