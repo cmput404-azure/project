1ffa2ab8f3e36842f9caf31120231ebac7b82797
xirui1219 <crystal7@ualberta.ca>
2024-10-13 00:28:45 -0600

    added the booleans for different types of listItems

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
-
+import ListItem from './components/ListItem/ListItem';
 
 export default function App() {
   return (
     <div className={styles.App}>
       Social distribution
+
+      <ListItem isRequest={true} isPost={false} isLike = {false} isFollowerList={false} />
+      <ListItem isRequest={false} isPost={true} isLike = {false} isFollowerList={false} />
+      <ListItem isRequest={false} isPost={false} isLike = {false} isFollowerList={true} />
+      <ListItem isRequest={false} isPost={true} isLike = {true} isFollowerList={false} />
+
     </div>
+
+
   );
 }
 
--- frontend/src/components/ListItem/ListItem.tsx
+++ frontend/src/components/ListItem/ListItem.tsx
@@ -1,16 +1,45 @@
 import styles from './ListItem.module.scss';
 
-export default function ListItem(){
-    return(
+interface ListItemProps {
+    isRequest: boolean;
+    isPost: boolean;
+    isLike: boolean;
+    isFollowerList: boolean;
+}
+
+export default function ListItem({
+    isRequest,
+    isPost,
+    isLike,
+    isFollowerList,
+}: ListItemProps) {
+    let additionalText = "";
+
+    if (isFollowerList) {
+        additionalText = "accepted your follow request";
+    } else if (isRequest) {
+        additionalText = "wants to follow you";
+    }
+    else if (isLike) {
+        additionalText = "liked your post";
+    } else if (isPost) {
+        additionalText = "shared a post with you";
+    }
+    return (
         <div className={styles.ListItemContainer}>
-        <div className = {styles.container}>
-            <img className = {styles.listImg} src='../images/Shiba-pfp.jpg' alt='pfp'/>
-            <div className = {styles.text}>
-            <h1>Garfield</h1>
-            <p>@Garfield890</p>
+            <div className={styles.container}>
+                <img className={styles.listImg} src='../images/Shiba-pfp.jpg' alt='pfp' />
+                <div className={styles.text}>
+                    <h1>Garfield
+                    <span className={styles.additionalText}>{additionalText}</span>
+                    </h1>
+                    <p>@Garfield890</p>
+                </div>
+
+                {isFollowerList ? <button>Unfollow</button> : null}
+                {isRequest ? <span><button>Accept</button> <button>Decline</button></span> : null}
+                {isPost ? <img className={styles.listImgPost} src='../images/Shiba-pfp.jpg' alt='pfp' /> : null}
             </div>
-            <button>Unfollow</button>
-        </div>
         </div>
     )
 }
