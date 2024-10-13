f5172b21b74bccc3c77baffd330cc2c653956503
Quin Nguyen <hnguyen6@ualberta.ca>
2024-10-13 00:08:34 -0600

    update inbox serializer

--- backend/azureDSN/serializers/inbox_serializer.py
+++ backend/azureDSN/serializers/inbox_serializer.py
@@ -1,23 +1,25 @@
 from rest_framework import serializers
 from rest_framework.serializers import *
 
-# from ..models import *
-# from .serializers import InboxItemSeriallizer
+from ..models import *
+from .serializers import InboxItemSeriallizer
 
-# class InboxSerializer(serializers.ModelSerializer):
-#     user = SerializerMethodField("getUserURL")
-#     items = SerializerMethodField("getItems")
-#     type = SerializerMethodField("getType")
+class InboxSerializer(serializers.ModelSerializer):
+    user = SerializerMethodField("getUserURL")
+    items = SerializerMethodField("getItems")
+    type = SerializerMethodField("getType")
 
-#     class Meta:
-#         model = Inbox
-#         fields = ("type", "user", "items")
+    class Meta:
+        model = Inbox
+        fields = ("type", "user", "items")
 
-#     def getUserURL(self, obj):
-#         return build_default_author_uri(obj=obj, request=self.context["request"], source = "inbox")
+    def getUserURL(self, obj):
+        # get user id in form of url
+        pass
 
-#     def getItems(self, obj):
-#         return InboxItemSerializer(obj.items.all().order_by("-id"), many=True, context=self.context).data
+    def getItems(self, obj):
+        # get the InboxItem object
+        pass
 
-#     def getType(self, obj):
-#         return "inbox"
+    def getType(self, obj):
+        return "inbox"
