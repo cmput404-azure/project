590958b39c2cb132c1825682d0407122ed90a4a4
Quin Nguyen <hnguyen6@ualberta.ca>
2024-10-14 13:59:25 -0600

    update urls to include inbox

--- backend/azureDSN/urls.py
+++ backend/azureDSN/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import *
 
 # urlpatterns contains all of the routes that this application supports routing for.
 # this routes traffic from polls/ to the index function that we defined earlier in the views file.
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("/api/authors/<uuid:user_id>/inbox/", InboxView.as_view(), name="inbox")
 ]
