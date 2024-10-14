e00449f76d8a9351fe17648f9dd097d17d238ab4
Khyl Nad <khylanad@gmail.com>
2024-10-14 13:58:47 -0600

    completed user profile header

--- frontend/src/components/UserProfile/UserProfile.tsx
+++ frontend/src/components/UserProfile/UserProfile.tsx
@@ -10,6 +10,7 @@ export default function UserProfile() {
           src="../images/yellowduck.png"
           alt="Profile"
         />
+
         <section className={styles.userInfoContainer}>
           <section className={styles.userInfo}>
             <section className={styles.userNameContainer}>
@@ -26,14 +27,28 @@ export default function UserProfile() {
           <span className={styles.userHandle}>@mr_ducky</span>
 
           <section className={styles.userStats}>
-            <span>Posts: 100</span>
-            <span>Followers: 100</span>
-            <span>Following: 100</span>
+            <span>
+              <p className={styles.count}>100</p> <p>posts</p>
+            </span>
+            <span>
+              <p className={styles.count}>100</p> <p>followers</p>
+            </span>
+            <span>
+              <p className={styles.count}>100</p> <p>following</p>
+            </span>
           </section>
+        </section>
+
+        <section className={styles.userProfileLink}>
+          <button className={styles.followButton}>Get Profile Link</button>
         </section>
       </section>
       <section className={styles.bioContainer}>
-        <p>this is the bio</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        </p>
       </section>
       <hr className={styles.horizontalLine} />
       <section className={styles.userPosts}>
