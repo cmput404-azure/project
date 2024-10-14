c2d6605078fe5ac222704658dda4756b5d6c349b
xirui1219 <crystal7@ualberta.ca>
2024-10-14 00:17:28 -0600

    changing listitem sizes + adding UserSearch component

--- frontend/src/components/UserSearch/UserSearch.tsx
+++ frontend/src/components/UserSearch/UserSearch.tsx
@@ -2,11 +2,14 @@ import styles from './UserSearch.module.scss';
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import ListItem from '../ListItem/ListItem';
 
-export default function UserSearch(){
-    return(
-        <div>
-            <div className = {styles.searchContainer}><i className="fa-solid fa-magnifying-glass"></i></div>
-            <ListItem ></ListItem>
+export default function UserSearch() {
+    return (
+        <div className={styles.componentWidth}>
+            <div className={styles.searchContainer}>
+                <i className="fa-solid fa-magnifying-glass"></i>
+                <input className={styles.searchInput} type="text" placeholder="Search..." />
+                </div>
+            <ListItem isRequest={false} isPost={false} isLike={false} isFollowerList={false}></ListItem>
         </div>
     )
 }
