58468b2c506a4c3f1045dee205e865f0eb6d1fc4
Quin Nguyen <hnguyen6@ualberta.ca>
2024-10-16 01:27:11 -0600

    update comment for inbox item model, clean urls, add util

--- backend/azureDSN/urls.py
+++ backend/azureDSN/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import *
+from . import views
+from .view import *
 
 # urlpatterns contains all of the routes that this application supports routing for.
 # this routes traffic from polls/ to the index function that we defined earlier in the views file.
 urlpatterns = [
+    path("", views.index, name="index"),
     path("/api/authors/<uuid:user_id>/inbox/", InboxView.as_view(), name="inbox")
 ]
