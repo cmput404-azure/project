f3ea8e7b03042dedc1ec2338f20c29a91b9baed5
natewu <36091727+natewu@users.noreply.github.com>
2024-10-16 13:53:04 -0600

    api layout

--- backend/azureDSN/urls.py
+++ backend/azureDSN/urls.py
@@ -4,5 +4,6 @@ from .views import *
 # urlpatterns contains all of the routes that this application supports routing for.
 # this routes traffic from polls/ to the index function that we defined earlier in the views file.
 urlpatterns = [
-    path("/api/authors/<uuid:user_id>/inbox/", InboxView.as_view(), name="inbox")
+    # path("/api/authors/<uuid:user_id>/inbox/", InboxView.as_view(), name="inbox")
+    path("", index, name="index"),
 ]
