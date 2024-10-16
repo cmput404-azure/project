994e835c024069893d265b1d557ad069933f2fcc
Kyle Quach <minhtriet.quach@dotdashmdp.com>
2024-10-15 19:45:16 -0600

    Finish PostBar/Author Post 2

--- frontend/src/components/PostBar/PostBar.tsx
+++ frontend/src/components/PostBar/PostBar.tsx
@@ -63,3 +63,27 @@ const PostBar: React.FC<PostBarProps> = ({ userImage, onAddClick }) => {
 export default PostBar;
 
 
+
+// import React from 'react';
+// import logo from './images/dog_icon.png';
+// import styles from './App.module.scss';
+
+// import PostBar from './components/PostBar/PostBar';
+// import AuthorPost from './components/AuthorPost/AuthorPost';
+
+// import PostCard from './components/PostCard/PostCard';
+// export default function App() {
+//   const handleAddClick = () => {
+//     console.log('Add button clicked');
+//   };
+
+//   return (
+//     <div className={styles.App}>
+//       Social distribution
+//       <PostBar userImage= {logo} onAddClick={handleAddClick} />
+//       <AuthorPost authorImage= {logo}  authorName = "Kyle Quach" userName="tmquach.meomeo" postText= "The authors personal bio goes here, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut." onAddClick={handleAddClick} />
+//     </div>
+
+
+//   );
+// }
