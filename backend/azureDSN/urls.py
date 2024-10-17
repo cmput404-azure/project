330be1bd27a401db0ed87bd5758a09b044f90958
xirui1219 <crystal7@ualberta.ca>
2024-10-17 13:45:56 -0600

    uncommented react url

--- backend/azureDSN/urls.py
+++ backend/azureDSN/urls.py
@@ -10,7 +10,7 @@ from azureDSN.views import index
 # urlpatterns contains all of the routes that this application supports routing for.
 # this routes traffic from polls/ to the index function that we defined earlier in the views file.
 urlpatterns = [
-    # re_path(r"^(?P<path>.*)$", index, {"document_root": settings.REACT_APP_BUILD_PATH}),
+    re_path(r"^(?P<path>.*)$", index, {"document_root": settings.REACT_APP_BUILD_PATH}),
   
     # path("/api/authors/<uuid:user_id>/inbox/", InboxView.as_view(), name="inbox")
     # path("", index, name="index"),
