ea4d886b251e33add6ef4d5118e2fe3a7d7efd27
Quin Nguyen <hnguyen6@ualberta.ca>
2024-10-17 02:35:34 -0600

    add api documentation path

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("", include("azureDSN.urls")),
     path('admin/', admin.site.urls),
+     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 # To serve media files during development
