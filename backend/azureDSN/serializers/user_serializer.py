250ae18f4182de1ee6ac49923481c2e716548038
erquette <erquette@ualberta.ca>
2024-10-16 21:24:24 -0600

    modified UserSerializer so that it returns the exact structure shown on the project website

--- backend/azureDSN/serializers/user_serializer.py
+++ backend/azureDSN/serializers/user_serializer.py
@@ -2,9 +2,17 @@ from rest_framework import serializers
 from ..models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    type = serializers.CharField(read_only=True)
+    id = serializers.UUIDField(source='uuid')
+    host = serializers.URLField()
+    displayName = serializers.CharField(source='display_name')
+    github = serializers.URLField()
+    profileImage = serializers.ImageField(source='profile_image')
+    page = serializers.URLField()
+
     class Meta:
         model = User
-        fields = ('uuid', 'host', 'display_name', 'page', 'github', 'profile_image', 'created_at', 'modified_at')
+        fields = ('type', 'id', 'host', 'displayName', 'github', 'profileImage', 'page')
 
     def create(self, validated_data):
         return User.objects.create(**validated_data)
