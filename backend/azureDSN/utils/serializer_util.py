eeef1398e2c7eccbec709e09c9783a357f9c7c0a
Quin Nguyen <hnguyen6@ualberta.ca>
2024-10-17 02:36:32 -0600

    update comment

--- backend/azureDSN/utils/serializer_util.py
+++ backend/azureDSN/utils/serializer_util.py
@@ -1,4 +1,9 @@
+'''
+This function converts an uuid into a url 
+'''
 def create_url_from_uuid(obj, request, type):
     uri = request.build_absolute_uri("/")
-    user_id = obj.user_id if type == "author" else obj.author.user_id
+    user_id = obj.user if type == "author" else obj.author.uuid
     return f"{uri}api/authors/{user_id}"
+
+
