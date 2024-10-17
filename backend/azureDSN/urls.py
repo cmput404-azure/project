eeb8c04aeb9ebcdfbef958f0155ace3d46ab48c1
Quin Nguyen <hnguyen6@ualberta.ca>
2024-10-16 22:32:44 -0600

    set up drf-spectacular for API documentation

--- backend/azureDSN/urls.py
+++ backend/azureDSN/urls.py
@@ -4,17 +4,19 @@ from django.contrib import admin
 from django.urls import path, re_path
 from django.conf import settings
 
-from azureDSN.views import serve_react
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+# from azureDSN.views import serve_react
 
 # urlpatterns contains all of the routes that this application supports routing for.
 # this routes traffic from polls/ to the index function that we defined earlier in the views file.
 urlpatterns = [
     re_path(r"^(?P<path>.*)$", index, {"document_root": settings.REACT_APP_BUILD_PATH}),
   
-    # path("/api/authors/<uuid:user_id>/inbox/", InboxView.as_view(), name="inbox")
     # path("", index, name="index"),
     # path("api/posts/{POST_FQID}/", PostView.as_view(), name="post"),
     path("api/authors/<uuid:author_serial>/", AuthorsView.as_view(), name="authors"),
     path("api/authors/<uuid:author_serial>/posts/<uuid:post_serial>/", AuthorPostView.as_view(), name="author_post"),
     # path("api/authors/", AuthorsView.as_view(), name="authors"),
+    path("/api/authors/<uuid:author_serial/inbox/", InboxView.as_view(), name="inbox")
 ]
