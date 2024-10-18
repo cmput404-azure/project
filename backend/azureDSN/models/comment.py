4ef55526f9907fdba715c2c3cc41b06183ac1807
Quin Nguyen <hnguyen6@ualberta.ca>
2024-10-18 00:31:42 -0600

    update comment model

--- backend/azureDSN/models/comment.py
+++ backend/azureDSN/models/comment.py
@@ -6,9 +6,9 @@ from .post import Post
 
 class Comment(models.Model):
     type = models.TextField(default="comment", editable=False)
-    comment_id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
-    user = models.JSONField(default=dict)
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.JSONField(default=dict) # can be local or remote
     comment = models.CharField(max_length=500)
     contentType = models.TextField(default='text/plain')
     created_at = models.DateTimeField("date commented", default=datetime.now)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
