b200098c4a42ff71dffbb6b6e6d203e07f241a65
natewu <36091727+natewu@users.noreply.github.com>
2024-10-19 02:42:59 -0600

    imports

--- backend/azureDSN/views/__init__.py
+++ backend/azureDSN/views/__init__.py
@@ -3,7 +3,4 @@ from .posts import PostView, AuthorPostView
 from .authors import AuthorsView, AuthorsCompleteView
 from .follow import FollowView, FollowCustomView
 from .inbox import InboxView
-from .follow import FollowGetView, FollowChangeView
-from .likes import SingleLikeView, AuthorLikesView
-from .comments import MultipleCommentsView
 from .likes import LikeView, AuthorLikesView, LikesView
