406726622b179f86d0e72b5daec8c04bac13a43a
Khyl Nad <khylanad@gmail.com>
2024-10-20 02:49:43 -0600

    styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,25 +4,24 @@ import ErrorPage from "./error-page";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 import Root from "./routes/Root";
 import UserProfile from "./components/UserProfile/UserProfile";
-import styles from './App.module.scss';
+import styles from "./App.module.scss";
 import HomePage from "./components/HomePage/HomePage";
 export default function App() {
   const nav = useNavigate();
 
   return (
     <div className={styles.App}>
-        <NavigationBar onClick={(item) => nav(`/${item}`)} isLoggedIn={false} />
+      <NavigationBar onClick={(item) => nav(`/${item}`)} isLoggedIn={false} />
 
-        <div className={styles.content}>
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/home" element={<HomePage/>} />
-            <Route path="/profile" element={<UserProfile />} />
-            <Route path="/settings" element={<Root />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </div>
+      <div className={styles.content}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/settings" element={<Root />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
-
