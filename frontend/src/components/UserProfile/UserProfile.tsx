12869c3e11d133427826517621a430181cc2bc5a
Khyl Nad <khylanad@gmail.com>
2024-10-20 00:02:56 -0600

    populate user profile with author data

--- frontend/src/components/UserProfile/UserProfile.tsx
+++ frontend/src/components/UserProfile/UserProfile.tsx
@@ -2,12 +2,32 @@ import { useState } from "react";
 import MiniPostCard from "../MiniPostCard/MiniPostCard";
 import styles from "./UserProfile.module.scss";
 import FollowList from "../FollowList/FollowList";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function UserProfile() {
-
+  const [authorData, setAuthorData] = useState(null);
   // FollowerList
   const [isFollowerListModalOpen, setIsFollowerListModalOpen] = useState(false);
   const [showFollowerList, setShowFollowerList] = useState(true);
+
+  // fetch the author data from the API when the component mounts
+  useEffect(() => {
+    const fetchAuthorData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8000/api/authors/5f577ee2-0ccc-49a4-b3cc-47a8aeb265df/"
+        );
+        setAuthorData(response.data); // Set the response data to state
+      } catch (error) {
+        console.error("Error fetching the author data", error);
+      }
+    };
+
+    fetchAuthorData();
+  }, []);
+
+  // fetch the authors posts from the API when the component mounts
 
   const openFollowers = () => {
     setShowFollowerList(true);
@@ -18,6 +38,11 @@ export default function UserProfile() {
     setShowFollowerList(false);
     setIsFollowerListModalOpen(true);
   };
+
+  if (!authorData) {
+    return <div>Loading...</div>; // Display a loading message until data is fetched
+  }
+
   return (
     <div className={styles.userProfileContainer}>
       <section className={styles.profileHeaderContainer}>
@@ -30,23 +55,30 @@ export default function UserProfile() {
         <section className={styles.userInfoContainer}>
           <section className={styles.userInfo}>
             <section className={styles.userNameContainer}>
-              <span className={styles.userName}>Mr. Ducky</span>
+              <span className={styles.userName}>{authorData.displayName}</span>
             </section>
             <section className={styles.buttonContainer}>
               <button className={styles.followButton}>Follow</button>
-              <a href="https://github.com" className={styles.githubButton}>
-                <img src="../images/githubIcon.png" />
+              <a
+                href={authorData.github}
+                className={styles.githubButton}
+                target="_blank" // Opens the link in a new tab
+                rel="noopener noreferrer"
+              >
+                <img src="../images/githubIcon.png" alt="GitHub" />
               </a>
             </section>
           </section>
 
-          <span className={styles.userHandle}>@mr_ducky</span>
+          <span className={styles.userHandle}>
+            @{authorData.displayName.toLowerCase().replace(" ", "_")}
+          </span>
 
           <section className={styles.userStats}>
             <span>
               <p className={styles.count}>100</p> <p>posts</p>
             </span>
-            <span onClick={openFollowers} style={{ cursor: 'pointer' }}>
+            <span onClick={openFollowers} style={{ cursor: "pointer" }}>
               <p className={styles.count}>100</p> <p>followers</p>
             </span>
             <FollowList
@@ -54,7 +86,7 @@ export default function UserProfile() {
               onClose={() => setIsFollowerListModalOpen(false)}
               isFollowerList={showFollowerList}
             />
-            <span onClick={openFollowing} style={{ cursor: 'pointer' }}>
+            <span onClick={openFollowing} style={{ cursor: "pointer" }}>
               <p className={styles.count}>100</p> <p>following</p>
             </span>
             <FollowList
@@ -69,6 +101,7 @@ export default function UserProfile() {
           <button className={styles.followButton}>Get Profile Link</button>
         </section>
       </section>
+
       <section className={styles.bioContainer}>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -76,62 +109,21 @@ export default function UserProfile() {
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
         </p>
       </section>
+
       <hr className={styles.horizontalLine} />
+
       <section className={styles.userPosts}>
         <MiniPostCard
           profilePic="../images/yellowduck.png"
-          userName="Mr. Ducky"
+          userName={authorData.displayName}
           postTime="11:11 PM"
           postContent="Excited to share my promotion to Software Developer III, massive thanks to @CorgiLabs!"
           postImage="../images/ducklings.jpg"
           likeCount={1523382}
           saveCount={250}
-          commentCount={10000} />
-        <MiniPostCard
-          profilePic="../images/yellowduck.png"
-          userName="Mr. Ducky"
-          postTime="11:11 PM"
-          postContent="Excited to share my promotion to Software Developer III, massive thanks to @CorgiLabs!"
-          postImage="../images/ducklings.jpg"
-          likeCount={1523382}
-          saveCount={250}
-          commentCount={10000} />
-        <MiniPostCard
-          profilePic="../images/yellowduck.png"
-          userName="Mr. Ducky"
-          postTime="11:11 PM"
-          postContent="Excited to share my promotion to Software Developer III, massive thanks to @CorgiLabs!"
-          postImage="../images/ducklings.jpg"
-          likeCount={1523382}
-          saveCount={250}
-          commentCount={10000} />
-        <MiniPostCard
-          profilePic="../images/yellowduck.png"
-          userName="Mr. Ducky"
-          postTime="11:11 PM"
-          postContent="Excited to share my promotion to Software Developer III, massive thanks to @CorgiLabs!"
-          postImage="../images/ducklings.jpg"
-          likeCount={1523382}
-          saveCount={250}
-          commentCount={10000} />
-        <MiniPostCard
-          profilePic="../images/yellowduck.png"
-          userName="Mr. Ducky"
-          postTime="11:11 PM"
-          postContent="Excited to share my promotion to Software Developer III, massive thanks to @CorgiLabs!"
-          postImage="../images/ducklings.jpg"
-          likeCount={1523382}
-          saveCount={250}
-          commentCount={10000} />
-        <MiniPostCard
-          profilePic="../images/yellowduck.png"
-          userName="Mr. Ducky"
-          postTime="11:11 PM"
-          postContent="Excited to share my promotion to Software Developer III, massive thanks to @CorgiLabs!"
-          postImage="../images/ducklings.jpg"
-          likeCount={1523382}
-          saveCount={250}
-          commentCount={10000} />
+          commentCount={10000}
+        />
+        {/* Add more MiniPostCard components as needed */}
       </section>
     </div>
   );
