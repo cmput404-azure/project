28297eb9844f993f43f8d1756756d667d6a3ad56
Kyle Quach <minhtriet.quach@dotdashmdp.com>
2024-10-19 20:53:04 -0600

    Add description and content

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -52,7 +52,7 @@ const HomePage = () => {
     <div className={styles.homePage}>
       {/* First Section: PostBar and Post Card */}
       <div className={styles.postSection}>
-        <PostBar userImage={logo} showButtonBar={true}/>
+        <PostBar userImage={logo} showButtonBar={false}/>
         <PostCard
           profilePic="https://via.placeholder.com/50"
           userName="John Doe"
