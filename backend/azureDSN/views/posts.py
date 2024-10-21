32c548fa4a5896de5b48d3b40d1d9735b5f97ca0
Khyl Nad <khylanad@gmail.com>
2024-10-20 18:50:28 -0600

    styling

--- backend/azureDSN/views/posts.py
+++ backend/azureDSN/views/posts.py
@@ -129,7 +129,6 @@ class AuthorPostView(APIView):
             return Response("Post does not exist.", status=404)
 
         # authenticate the user
-        
         if post.user.uuid != author_serial:
             return Response("You are not the author of this post.", status=403)
         else:
