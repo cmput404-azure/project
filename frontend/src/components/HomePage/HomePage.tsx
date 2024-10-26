0e5dbc2af546018ad591d86ed6bd58cbfb2690b9
Quin Nguyen <hnguyen6@ualberta.ca>
2024-10-26 15:23:45 -0600

    fix props passed to post card

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -117,16 +117,9 @@ const HomePage = () => {
         {displayedPosts.map((post) => (
           <PostCard
             key={post.id}
-            profilePic={post.author.profileImage}
-            userName={post.author.displayName}
-            postTime={new Date(post.published).toLocaleString()}
-            postContent={post.content}
-            postImage={post.has_image ? post.image : ""}
-            likeCount={post.likes.length}
-            saveCount={0}
-            commentCount={post.comments.length}
+            post_obj = {post}
             onCommentButtonClick={() => handleCommentButtonClick(post)}
-            onClick={() => handleCommentButtonClick(post)}
+            // onClick={() => handleCommentButtonClick(post)}
           />
         ))}
       </div>
@@ -154,17 +147,7 @@ const HomePage = () => {
             // pass in the selected post for the modal to display
             <PostCard
               key={selectedPost.id}
-              profilePic={
-                selectedPost.author?.profileImage ||
-                `https://ui-avatars.com/api/?background=random&name=${selectedPost.author?.displayName}`
-              }
-              userName={selectedPost.author.displayName}
-              postTime={new Date(selectedPost.published).toLocaleString()}
-              postContent={selectedPost.content}
-              postImage={""}
-              likeCount={0}
-              saveCount={0}
-              commentCount={0}
+              post_obj = {selectedPost}
             />
           ) : null
         }
