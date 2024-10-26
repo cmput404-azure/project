63f6a35e54e05a389937a21885c260e7bfaeacb3
Quin Nguyen <hnguyen6@ualberta.ca>
2024-10-26 13:52:21 -0600

    update contentType for posts

--- frontend/src/models/modelTypes.tsx
+++ frontend/src/models/modelTypes.tsx
@@ -6,9 +6,11 @@ export enum InboxItemType {
 }
 
 export enum ContentType {
-    BASE64 = "application/base64",
+    BASE64 = "image/base64",
+    JPEG = "image/jpeg;base64",
     MARKDOWN = "text/markdown",
     PLAIN = "text/plain",
+    PNG = "image/png;base64",
 }
 
 export enum VisibilityChoices {
