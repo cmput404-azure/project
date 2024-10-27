de8a00e3fc54d82b7bd745d3300886186b74c0c9
Kyle Quach <minhtriet.quach@dotdashmdp.com>
2024-10-27 00:58:43 -0600

    Comment Name Convension

--- backend/azureDSN/models/comment.py
+++ backend/azureDSN/models/comment.py
@@ -6,10 +6,9 @@ from .post import Post
 
 class Comment(models.Model):
     type = models.TextField(default="comment", editable=False)
-    author = models.JSONField(default=dict, db_column="user" ) # can be local or remote
+    user = models.JSONField(default=dict, db_column="user" ) # can be local or remote
     comment = models.CharField(max_length=500)
     contentType = models.TextField(default='text/plain')
     created_at = models.DateTimeField("date commented", default=datetime.now)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)  # The ID of comment 
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    likes =  models.JSONField(default=dict)
