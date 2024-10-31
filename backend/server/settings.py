b1d7cdafa04b30fbc1ba67d0d35b95f6bcf3706e
erquette <erquette@ualberta.ca>
2024-10-30 22:13:44 -0600

    added 127.0.0.1 on cors settings

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = False  
 CORS_ALLOW_CREDENTIALS = True 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',  
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
     'http://ui19:3000'
 ]
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
