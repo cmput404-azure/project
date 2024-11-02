767f8fca0676fbf947f9f677417f78f5f91a88e7
Khyl Nad <khylanad@gmail.com>
2024-11-02 16:49:28 -0600

    styling complete

--- frontend/src/components/Auth/Auth.tsx
+++ frontend/src/components/Auth/Auth.tsx
@@ -2,189 +2,242 @@ import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import { Button } from "@mui/material";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 import { login } from "../../util/auth/login";
 import { logout } from "../../util/auth/checkauth";
-import styles from './Auth.module.scss';
+import styles from "./Auth.module.scss";
 import { useAuth } from "../../state";
 import authService from "../../service/auth";
+import styled from "@mui/material/styles/styled";
+
+const StyledTextField = styled(TextField)({
+  "& label": {
+    color: "#ffffff !important",
+  },
+
+  "& input": {
+    color: "white !important",
+    backgroundColor: "#2b2b2b", // lighter gray background inside the input
+    padding: "10px", // Optional: to add some padding around the text
+    borderRadius: "5px", // Optional: to add rounded corners
+  },
+  "& textarea": {
+    color: "white !important",
+  },
+
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      border: "none",
+      boxShadow: "0 4px 7px rgba(0, 0, 0, 0.45)",
+    },
+    "&:hover fieldset": {
+      border: "1px solid",
+      borderColor: "white !important",
+    },
+    "&.Mui-focused fieldset": {
+      border: "1px solid",
+      borderColor: "#70ffaf !important",
+    },
+  },
+
+  "& .MuiFormHelperText-root": {
+    color: "#ffffff",
+    "&.Mui-error": {
+      color: "#dc3545",
+    },
+  },
+});
 
 function Auth() {
-   const [isRegister, setIsRegister] = useState(false);
-   const [username, setUsername] = useState("");
-   const [password, setPassword] = useState("");
-   const [name, setName] = useState("");
-   const [email, setEmail] = useState("");
-   const [confirmPassword, setConfirmPassword] = useState("");
-   const [githubUsername, setGithubUsername] = useState("");
-   const [error, setError] = useState("");
-   const navigate = useNavigate();
-   const authProvider = useAuth();
+  const [isRegister, setIsRegister] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [githubUsername, setGithubUsername] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const authProvider = useAuth();
 
-   const location = useLocation();
+  const location = useLocation();
 
-   useEffect(() => {
-      // Check if user is already logged in, return to prev page
-      const from = location.state?.from?.pathname || '/';
+  useEffect(() => {
+    // Check if user is already logged in, return to prev page
+    const from = location.state?.from?.pathname || "/";
 
-      if (authProvider.isAuthenticated) {
-         navigate(from);
-      }
-   }, [authProvider.isAuthenticated, authProvider.loading, navigate, location]);
+    if (authProvider.isAuthenticated) {
+      navigate(from);
+    }
+  }, [authProvider.isAuthenticated, authProvider.loading, navigate, location]);
 
-   const handleSubmit = async (e) => {
-      e.preventDefault();
-      setError("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
 
-      // Validation
-      if (!username || !password || (isRegister && (!name || !email))) {
-         setError("Please fill all required fields.");
-         return;
-      }
-      if (isRegister && password !== confirmPassword) {
-         setError("Passwords do not match.");
-         return;
-      }
+    // Validation
+    if (!username || !password || (isRegister && (!name || !email))) {
+      setError("Please fill all required fields.");
+      return;
+    }
+    if (isRegister && password !== confirmPassword) {
+      setError("Passwords do not match.");
+      return;
+    }
 
-      if (isRegister) {
-         const response = await authService.register({ username, password, email, name, githubUsername });
+    if (isRegister) {
+      const response = await authService.register({
+        username,
+        password,
+        email,
+        name,
+        githubUsername,
+      });
 
-         if (response) {
-            alert("Registration successful! Please log in.");
-            setIsRegister(false); // Switch back to login form
-          } else {
-            setError("Registration failed. Username might already be taken.");
-          }
-
+      if (response) {
+        alert("Registration successful! Please log in.");
+        setIsRegister(false); // Switch back to login form
       } else {
-         login(username, password)
-         .then((response) => {
-            authProvider.setIsAuthenticated(true);
-            authProvider.setUser(response.user);
-            navigate("/");
-         })
-         .catch((error) => {
-            console.log(error);
-            setError(error.response.data.message);
-         });
+        setError("Registration failed. Username might already be taken.");
       }
+    } else {
+      login(username, password)
+        .then((response) => {
+          authProvider.setIsAuthenticated(true);
+          authProvider.setUser(response.user);
+          navigate("/");
+        })
+        .catch((error) => {
+          console.log(error);
+          setError(error.response.data.message);
+        });
+    }
 
-      resetFields();
-   };
+    resetFields();
+  };
 
-   const resetFields = () => {
-      setUsername("");
-      setPassword("");
-      setName("");
-      setGithubUsername("");
-      setEmail("");
-      setConfirmPassword("");
-   };
+  const resetFields = () => {
+    setUsername("");
+    setPassword("");
+    setName("");
+    setGithubUsername("");
+    setEmail("");
+    setConfirmPassword("");
+  };
 
-   return (
-      <div className={styles.auth}>
-         <div className={styles.auth__container}>
-         <div className={styles.auth__container__header}>
-            <h1>{isRegister ? "Register" : "Login"}</h1>
-            <p
-               className={styles.check}
-               onClick={() => setIsRegister(!isRegister)}
-            >
-               {isRegister ? "Login instead" : "Register instead"}
-            </p>
-         </div>
+  return (
+    <div className={styles.auth}>
+      <div className={styles.auth__container}>
+        <div className={styles.auth__container__header}>
+          <h1>{isRegister ? "Register" : "Login"}</h1>
+        </div>
 
-         <form className={styles.auth__container__form} onSubmit={handleSubmit}>
-            <TextField
-               label="Username"
-               variant="outlined"
-               size="small"
-               value={username}
-               onChange={(e) => setUsername(e.target.value)}
-               required
+        <form className={styles.auth__container__form} onSubmit={handleSubmit}>
+          <StyledTextField
+            label="Username"
+            variant="outlined"
+            size="small"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          {isRegister && (
+            <StyledTextField
+              label="Name"
+              variant="outlined"
+              size="small"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
             />
-            {isRegister && (
-               <TextField
-               label="Name"
-               variant="outlined"
-               size="small"
-               value={name}
-               onChange={(e) => setName(e.target.value)}
-               required
-               />
-            )}
-            {isRegister && (
-               <TextField
-               label="Email"
-               variant="outlined"
-               size="small"
-               value={email}
-               onChange={(e) => setEmail(e.target.value)}
-               required
-               />
-            )}
-            {isRegister && (
-               <TextField
-               label="GitHub Username"
-               variant="outlined"
-               size="small"
-               value={githubUsername}
-               onChange={(e) => setGithubUsername(e.target.value)}
-               />
-            )}
-            <TextField
-               label="Password"
-               variant="outlined"
-               size="small"
-               type="password"
-               value={password}
-               onChange={(e) => setPassword(e.target.value)}
-               required
+          )}
+          {isRegister && (
+            <StyledTextField
+              label="Email"
+              variant="outlined"
+              size="small"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
-            {isRegister && (
-               <TextField
-               label="Confirm Password"
-               variant="outlined"
-               size="small"
-               type="password"
-               value={confirmPassword}
-               onChange={(e) => setConfirmPassword(e.target.value)}
-               required
-               />
-            )}
-            <div className={styles.auth__container__footer}>
-               <button type="submit">Submit</button>
-               {error && <p className={styles.error}>{error}</p>}
-            </div>
-         </form>
-         </div>
+          )}
+          {isRegister && (
+            <StyledTextField
+              label="GitHub Username"
+              variant="outlined"
+              size="small"
+              value={githubUsername}
+              onChange={(e) => setGithubUsername(e.target.value)}
+            />
+          )}
+          <StyledTextField
+            label="Password"
+            variant="outlined"
+            size="small"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          {isRegister && (
+            <StyledTextField
+              label="Confirm Password"
+              variant="outlined"
+              size="small"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+          )}
+          <div className={styles.auth__container__footer}>
+            <button type="submit">Submit</button>
+            {error && <p className={styles.error}>{error}</p>}
+          </div>
+        </form>
+        <section className={styles.auth__container__footer}>
+          <p className={styles.auth__account__or__not}>
+            {isRegister ? "Already have an account?" : "Don't have an account?"}
+          </p>
+          <p
+            className={styles.check}
+            onClick={() => setIsRegister(!isRegister)}
+          >
+            {isRegister ? "Login instead" : "Register instead"}
+          </p>
+        </section>
       </div>
-   );
+    </div>
+  );
 }
 
 export function Logout() {
-   const navigate = useNavigate();
-   const authProvider = useAuth();
+  const navigate = useNavigate();
+  const authProvider = useAuth();
 
-   return (
-      <div className={styles.auth}>
-         <div className={styles.auth__container}>
-            <div className={styles.auth__container__header}>
-               <h1>Logout</h1>
-            </div>
-            <div className={styles.auth__container__form}>
-               <Button variant="contained" color="primary" onClick={() => {
-                  logout().then(() => {
-                     navigate("/");
-                     authProvider.logout();
-                  });
-               }}>
-                  Logout
-               </Button>
-            </div>
-         </div>
+  return (
+    <div className={styles.auth}>
+      <div className={styles.auth__container}>
+        <div className={styles.auth__container__header}>
+          <h1>Logout</h1>
+        </div>
+        <div className={styles.auth__container__form}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              logout().then(() => {
+                navigate("/");
+                authProvider.logout();
+              });
+            }}
+          >
+            Logout
+          </Button>
+        </div>
       </div>
-   );
+    </div>
+  );
 }
 
 export default Auth;
