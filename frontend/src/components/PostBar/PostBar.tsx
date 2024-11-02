da0b2607842ee4f05b68634e82b3a3f21ff4ae51
Kyle Quach <minhtriet.quach@dotdashmdp.com>
2024-11-01 23:37:18 -0600

    Fixing UI Postbar

--- frontend/src/components/PostBar/PostBar.tsx
+++ frontend/src/components/PostBar/PostBar.tsx
@@ -253,7 +253,7 @@ const PostBar: React.FC<PostBarProps> = ({ fetchPosts, author }) => {
           fullWidth
           variant="standard"
           size="small"
-          placeholder="Start typing..."
+          placeholder="Enter a title for your new post..."
           value={title}
           onChange={handleTitleChange}
           autoComplete='off'
