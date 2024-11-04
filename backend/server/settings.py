1b3325d12456bbb650be981a3665bc28805a69fb
natewu <36091727+natewu@users.noreply.github.com>
2024-11-04 13:34:37 -0700

    cors

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -35,8 +35,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://ui19:3000'
+    'https://azuredsn-889a4fb9b2bb.herokuapp.com'
 ]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://azuredsn-889a4fb9b2bb.herokuapp.com']
 
 # Application definition
 
