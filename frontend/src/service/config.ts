47855c6b8f6b945fbdb1c32f66edebd2892984c8
nate <36091727+natewu@users.noreply.github.com>
2024-11-04 07:13:31 -0700

    Update config.ts

--- frontend/src/service/config.ts
+++ frontend/src/service/config.ts
@@ -8,7 +8,7 @@ axios.defaults.xsrfHeaderName = "x-csrftoken";
 const csrfToken = getCsrfToken();
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://azuredsn-889a4fb9b2bb.herokuapp.com/",
   timeout: 10000,
   headers: {
     "x-csrftoken": csrfToken,
