8cae3169fd646ad15dfbde145e63b91c1c5038e7
nate <36091727+natewu@users.noreply.github.com>
2024-11-04 15:48:25 -0700

    Update settings.py

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_URL = "https://azuredsn-889a4fb9b2bb.herokuapp.com/"
+BASE_URL = "https://azuredsn-889a4fb9b2bb.herokuapp.com"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
