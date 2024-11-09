58cf1862d1285bab2c9c20c59d48ddec6dcd29d0
erquette <erquette@ualberta.ca>
2024-11-09 12:49:07 -0700

    update like endpoint to use comment fqid

--- backend/azureDSN/urls.py
+++ backend/azureDSN/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     # Likes on Posts or Comments
     path("api/authors/<uuid:author_serial>/posts/<uuid:post_serial>/likes/", LikesView.as_view(), name="get_likes_by_serial"),
     path("api/posts/<path:post_fqid>/likes/", LikesView.as_view(), name="get_likes_by_fqid"),
-    path("api/authors/<uuid:author_serial>/posts/<uuid:post_serial>/comments/<uuid:comment_serial>/likes", LikesView.as_view(), name="get_comment_likes"),
+    path("api/authors/<uuid:author_serial>/posts/<uuid:post_serial>/comments/<path:comment_fqid>/likes", LikesView.as_view(), name="get_comment_likes"),
     
     # Comments API
     path('api/authors/<uuid:author_serial>/posts/<uuid:post_serial>/comments/',MultipleCommentsView.as_view(),name='comments_by_serial'),
