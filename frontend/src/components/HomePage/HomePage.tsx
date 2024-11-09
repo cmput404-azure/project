485ecb8300e9a197aca9bade03f50f8d4ddc781a
erquette <erquette@ualberta.ca>
2024-11-08 18:03:00 -0700

    implemented load more button in both stream

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress, Modal } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 // HomePage.jsx
 import { useEffect, useState } from "react";
 
@@ -7,7 +7,6 @@ import Post from "../Post/Post";
 import PostBar from "../PostBar/PostBar";
 import PublicIcon from "@mui/icons-material/Public";
 import { api } from "../../service/config";
-import { decodeBase64ToUrl } from "../../util/rendering/decodeBase64ToUrl";
 import stream from "../../service/stream";
 import styles from "./HomePage.module.scss";
 import { useAuth } from "../../state";
@@ -15,10 +14,16 @@ import { useAuth } from "../../state";
 type ViewType = "all" | "unlisted_friends-only";
 const HomePage = () => {
   const [publicPosts, setPublicPosts] = useState<any[]>([]);
-  const [nonPublicPosts, setNonPublicPosts] = useState<any[]>([]);
+  const [privatePosts, setPrivatePosts] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [user, setUser] = useState<any>(null);
+  const [activeFilterPost, setActiveFilterPost] = useState<ViewType>("all");
+  const [publicPage, setPublicPage] = useState(1); // so we can use the pagination feature
+  const [privatePage, setPrivatePage] = useState(1);
+  const [totalPublicPages, setTotalPublicPages] = useState(0);
+  const [totalPrivatePages, setTotalPrivatePages] = useState(0);
+  const pageSize = 5;
   const authProvider = useAuth();
 
   const [isUserLoading, setIsUserLoading] = useState(true);
@@ -46,26 +51,39 @@ const HomePage = () => {
     fetchUser();
   }, [authProvider.user]); // This effect runs when authProvider.user changes
 
-  const fetchPosts = async () => {
+  const fetchPosts = async (
+    publicPage = 1,
+    privatePage = 1,
+  ) => {
     if (isUserLoading) return;
-
+  
     try {
-      // Fetch paginated data for the requested page
-      const publicPosts = await stream.getStream();
-      const privatePosts = await stream.getStream(true); // this returns PaginatedResponse type
+      const publicResponse = await stream.getStream(false, publicPage);
+      const privateResponse = await stream.getStream(true, privatePage);
+      
+      setPublicPosts(prevPosts => {
+        const existingIds = new Set(prevPosts.map(post => post.id));
+        const newPublicPosts = publicResponse.src.filter(post => !existingIds.has(post.id));
+        const latestExistingDate = new Date(Math.max(...prevPosts.map(post => new Date(post.published).getTime())));
+        const newerPosts = newPublicPosts.filter(post => new Date(post.published).getTime() > latestExistingDate.getTime()); // the ones created from PostBar
+        const filteredPosts = newPublicPosts.filter(post => !newerPosts.some(newPost => newPost.id === post.id)); // don't want duplicate
+        return [...newerPosts, ...prevPosts, ...filteredPosts];
+      });
 
-      const decodedPublicPosts = decodeBase64ToUrl(publicPosts.src);
-      const decodedNonPublicPosts = decodeBase64ToUrl(privatePosts.src);
+      setPrivatePosts(prevPosts => {
+        const existingIds = new Set(prevPosts.map(post => post.id));
+        const newPrivatePosts = privateResponse.src.filter(post => !existingIds.has(post.id));
+        const latestExistingDate = new Date(Math.min(...prevPosts.map(post => new Date(post.published).getTime())));
+        const newerPosts = newPrivatePosts.filter(post => new Date(post.published).getTime() > latestExistingDate.getTime());
+        const filteredPosts = newPrivatePosts.filter(post => !newerPosts.some(newPost => newPost.id === post.id));
+        return [...newerPosts, ...prevPosts, ...filteredPosts];
+      });
 
-      setPublicPosts(decodedPublicPosts);
-      setNonPublicPosts(decodedNonPublicPosts);
+      setTotalPublicPages(Math.ceil(publicResponse.count / pageSize));
+      setTotalPrivatePages(Math.ceil(privateResponse.count / pageSize));
+  
       setIsLoading(false);
 
-      return {
-        publicPosts: decodedPublicPosts,
-        nonPublicPosts: decodedNonPublicPosts
-      }
-      
     } catch (err) {
       console.log(err);
       setError("Failed to fetch posts. Please try again.");
@@ -73,16 +91,30 @@ const HomePage = () => {
   };
 
   useEffect(() => {
-    fetchPosts();
-    const interval = setInterval(fetchPosts, 60000);
+    fetchPosts(publicPage, privatePage); // fetch initial
+    const interval = setInterval(() => {
+      fetchPosts(publicPage, privatePage);
+    }, 10000);
     return () => clearInterval(interval); // Clean up the interval on component unmount
-  }, [isUserLoading]);
+  }, [isUserLoading, privatePage, publicPage]);
 
-  const [activeFilterPost, setActiveFilterPost] = useState<ViewType>("all");
+  const nextPublicPage = async () => {
+    if (isLoading || publicPage >= totalPublicPages) return;
+    setPublicPage(prevPage => prevPage + 1);
+  }
+
+  const nextPrivatePage = async () => {
+    if (isLoading || privatePage >= totalPrivatePages) return;
+    setPrivatePage(prevPage => prevPage + 1);
+  }
+
   function handleFilterPost(icon: ViewType) {
     setActiveFilterPost(icon);
-    fetchPosts();
   }
+
+  // const hasPostUpdated = (existingPost, newPost) => {
+  //   return existingPost.content !== newPost.content;
+  // };
 
   if (isLoading)
     return (
@@ -92,8 +124,7 @@ const HomePage = () => {
     );
   if (error) return <p>{error}</p>;
 
-  const displayedPosts =
-    activeFilterPost === "all" ? publicPosts : nonPublicPosts;
+  const displayedPosts = activeFilterPost === "all" ? publicPosts : privatePosts;
 
   return (
     <div className={styles.homePage}>
@@ -129,6 +160,27 @@ const HomePage = () => {
           <div className={styles.noPosts}>
             <p>There are no posts on this node 🫨</p>
           </div>
+        )}
+        {activeFilterPost === "all" && publicPage < totalPublicPages && (
+          <Button
+            variant="contained"
+            onClick={nextPublicPage}
+            disabled={isLoading}
+            sx={{ marginTop: "1rem", backgroundColor: "#70ffaf", color: "black" }}
+          >
+            {isLoading ? <CircularProgress size={24} sx={{ color: "#70ffaf" }} /> : "Load More"}
+          </Button>
+        )}
+
+        {activeFilterPost === "unlisted_friends-only" && privatePage < totalPrivatePages && (
+          <Button
+            variant="contained"
+            onClick={nextPrivatePage}
+            disabled={isLoading}
+            sx={{ marginTop: "1rem", backgroundColor: "#70ffaf", color: "black" }}
+          >
+            {isLoading ? <CircularProgress size={24} sx={{ color: "#70ffaf" }} /> : "Load More"}
+          </Button>
         )}
       </div>
     </div>
