a7cbe5cf6d7c3fd2a4e14ae052bf55c8c387a783
erquette <erquette@ualberta.ca>
2024-11-09 21:08:59 -0700

    raise bugs

--- frontend/src/components/ListItem/ListItem.tsx
+++ frontend/src/components/ListItem/ListItem.tsx
@@ -161,11 +161,13 @@ export default function ListItem({
 
   let additionalText = "";
   if (isRequest) additionalText = "wants to follow you";
-  else if (isPost) additionalText = `posted a post titled: ${postObj.title}`;
+  else if (isShare) additionalText = `shared a post with you titled: ${postObj.title}`;
   else if (isLike) additionalText = `liked your post titled: ${postObj.title}`;
   else if (isComment) additionalText = `commented on your post titled: ${postObj.title}`;
-  else if (isShare) additionalText = `shared a post with you titled: ${postObj.title}`;
+  
+  // notification fails to differetiate these posts below
   else if (isDeletedPost) additionalText = `deleted their post titled: ${postObj.title}`;
+  else if (isPost) additionalText = `posted a post titled: ${postObj.title}`;
   else if (isUpdatedPost) additionalText = `updated their post titled: ${postObj.title}`;
 
   return (
