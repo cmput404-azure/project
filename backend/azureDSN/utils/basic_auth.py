35b9220090049317e3375aa6234836d67e483d2f
erquette <erquette@ualberta.ca>
2024-11-15 19:39:11 -0700

    fixed permission check

--- backend/azureDSN/utils/basic_auth.py
+++ backend/azureDSN/utils/basic_auth.py
@@ -1,43 +1,36 @@
-from urllib.parse import urlparse
+from rest_framework.authentication import get_authorization_header
 from rest_framework.permissions import BasePermission
 from base64 import b64decode
 from ..models import NodeUser
-import os
-
-class IsAuthenticatedNode(BasePermission):
+class TokenOrBasicAuthPermission(BasePermission):
     def has_permission(self, request, view):
-        # Allow local requests without authentication
-        base_url = os.getenv('BASE_URL', '')
-        origin_header = request.headers.get('Origin', '')
+        # Token has higher precedence over BasicAuth
+        csrf_token = request.headers.get("X-CSRFToken") if request.headers.get("X-CSRFToken") else request.COOKIES.get('csrftoken')
+        print(csrf_token)
 
-        def ensure_scheme(url):
-            if not url.startswith(('http://', 'https://')):
-                return f'http://{url}'
-            return url
-
-        parsed_base_url = urlparse(ensure_scheme(base_url))
-        parsed_origin = urlparse(ensure_scheme(origin_header))
-
-        # Only allow unauthenticated requests that exactly match BASE_URL
-        if origin_header and parsed_origin.netloc == parsed_base_url.netloc:
+        if csrf_token:
             return True
 
-        auth_header = request.headers.get('Authorization')
-        if not auth_header or not auth_header.startswith('Basic '):
-            return False
+        # Check Basic Authentication for remote requests
+        auth_header = get_authorization_header(request).split()
+        if len(auth_header) == 2 and auth_header[0].lower() == b"basic":
+            return self._is_valid_basic_auth(auth_header[1].decode())
+        
+        return False
 
-        # Decode Basic Auth credentials
+    def _is_valid_basic_auth(self, auth_value):
+        """
+            Validate Basic Auth credentials (for remote requests)
+        """
         try:
-            encoded_credentials = auth_header.split(' ')[1]
-            decoded_credentials = b64decode(encoded_credentials).decode('utf-8')
+            # Decode Basic Auth credentials
+            decoded_credentials = b64decode(auth_value).decode('utf-8')
             username, password = decoded_credentials.split(':')
-        except (IndexError, ValueError):
+            
+            # Validate credentials with data stored in database
+            node = NodeUser.objects.get(username=username)
+            if node.password == password and node.is_authenticated:
+                return True
             return False
-
-        origin = f"{parsed_origin.scheme}://{parsed_origin.netloc}"
-        # Validate credentials in the database
-        try:
-            node = NodeUser.objects.get(host=origin)
-            return node.username == username and node.password == password and node.is_authenticated
-        except NodeUser.DoesNotExist:
+        except Exception as e:
             return False
