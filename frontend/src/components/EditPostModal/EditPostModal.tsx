fea3e65ccb9e30481be01001ff9c9bebaa97940f
Khyl Nad <khylanad@gmail.com>
2024-11-16 04:12:50 -0700

    fixed visibility comparison

--- frontend/src/components/EditPostModal/EditPostModal.tsx
+++ frontend/src/components/EditPostModal/EditPostModal.tsx
@@ -75,7 +75,7 @@ export default function EditPostModal({
     if (
       title === post.title &&
       content === post.content &&
-      visibility === visibility
+      visibility === post.visibility
     ) {
       setDisabled(true);
     } else {
