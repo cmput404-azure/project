faea221ccaa35c20f0d5309c75c161baeac30809
erquette <erquette@ualberta.ca>
2024-11-17 06:18:31 -0700

    fixed like render on remote posts on local stream

--- backend/azureDSN/views/stream.py
+++ backend/azureDSN/views/stream.py
@@ -1,4 +1,4 @@
-from urllib.parse import quote
+from urllib.parse import quote, urlparse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -44,36 +44,32 @@ class PublicStreamView(APIView):
                     visibility = remote_payload.get("visibility")
                     
                     if visibility == "PUBLIC" and (item.post_status == None or item.post_status.upper() != "DELETE"):
-                        if post_id and post_id not in remote_posts: # Add if this post hasn't been added
-                              # Commented out since not sure how to resolve
-#                             author_host = remote_payload["author"]["host"]
-#                             post_fqid = f"{remote_payload['author']['id']}/post/{post_id}"
+                        author_host = urlparse(remote_payload["author"]["host"])
+                        base_author_host = f"{author_host.scheme}://{author_host.netloc}"
+                        post_uuid = post_id.rstrip('/').split('/')[-1]
+                        author_fqid = remote_payload["author"]["id"]
+                        author_uuid = author_fqid.rstrip('/').split('/')[-1]
 
-#                             encoded_post_fqid = quote(post_fqid, safe="")
-#                             get_post_url = f"{author_host}posts/{encoded_post_fqid}/"
-                            
-#                             try:
-#                                 # Perform the GET request
-#                                 response = requests.get(
-#                                     get_post_url,
-#                                     auth=HTTPBasicAuth(os.getenv('NODE_USERNAME'), os.getenv('NODE_PASSWORD'))
-#                                 )
+                        get_post_url = f"{base_author_host}/api/authors/{author_uuid}/posts/{post_uuid}/"
+                        try:
+                            # Perform the GET request
+                            response = requests.get(
+                                get_post_url,
+                                auth=HTTPBasicAuth(os.getenv('NODE_USERNAME'), os.getenv('NODE_PASSWORD'))
+                            )
 
-#                                 if response.status_code == 200:
-#                                     post_data = response.json()  
-#                                     if post_id not in remote_posts:  
-#                                         remote_posts[post_id] = post_data
-#                                 else:
-#                                     print(f"Failed to fetch post. Status code: {response.status_code}")
-#                             except Exception as e:
-#                                 print(f"Error fetching remote post {get_post_url}: {e}")
-                           
-                            print(f"Added post with id: {post_id}")
-                            remote_posts[post_id] = remote_payload
-                        else:
-                            if item.post_status and item.post_status.upper() == "UPDATE":
-                                # There's a newer version of this post
-                                remote_posts[post_id] = remote_payload
+                            if response.status_code == 200:
+                                post_data = response.json()
+
+                                if post_id not in remote_posts: # Add if this post hasn't been added
+                                    remote_posts[post_id] = post_data
+                                elif item.post_status and item.post_status.upper() == "UPDATE":
+                                    # There's a newer version of this post
+                                    remote_posts[post_id] = post_data
+                            else:
+                                print(f"Failed to fetch post. Status code: {response.status_code}")
+                        except Exception as e:
+                            print(f"Error fetching remote post {get_post_url}: {e}")
 
         unique_remote_posts = list(remote_posts.values())
 
