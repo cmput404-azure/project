83331db869de9c1977da6f87e4995f8300135fec
erquette <erquette@ualberta.ca>
2024-11-17 22:25:35 -0700

    remote private posts now visible on stream

--- backend/azureDSN/views/stream.py
+++ backend/azureDSN/views/stream.py
@@ -201,26 +201,33 @@ class AuthStreamView(APIView):
 
                         author_host = urlparse(remote_payload["author"]["host"])
                         base_author_host = f"{author_host.scheme}://{author_host.netloc}"
-                        post_uuid = post_id.rstrip('/').split('/')[-1]
-                        author_fqid = remote_payload["author"]["id"]
-                        author_uuid = author_fqid.rstrip('/').split('/')[-1]
-                        get_post_url = f"{base_author_host}/api/authors/{author_uuid}/posts/{post_uuid}/"
+                        
+                        encoded_url = quote(post_id, safe='')
+                        get_post_url = f"{base_author_host}/api/posts/{encoded_url}"
+
                         try:
                             response = requests.get(
                                 get_post_url,
                                 auth=HTTPBasicAuth(os.getenv('NODE_USERNAME'), os.getenv('NODE_PASSWORD'))
                             )
+                            print(response.status_code)
                             if response.status_code == 200:
                                 post_data = response.json()
-                                remote_posts[post_id] = post_data
+
+                                if post_id not in remote_posts: # Add if this post hasn't been added
+                                    remote_posts[post_id] = post_data
+                                elif item.post_status and item.post_status.upper() == "UPDATE":
+                                    # There's a newer version of this post
+                                    remote_posts[post_id] = post_data
 
                             processed_posts[post_id] = item.post_status
                         except Exception as e:
                             print(f"Error fetching post {post_id}: {e}")
 
             combined_posts = serialized_local_posts.copy()
-            for remote_post in remote_posts:
-                combined_posts.append(remote_post)
+            
+            for post_id, post_data in remote_posts.items():
+                combined_posts.append(post_data)
 
             """
                 In this stream, there is also a case where user also see posts shared by people they follow
