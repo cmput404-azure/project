ccfec6929cc85c0bf009ff97a4428c97bfefc2ec
erquette <erquette@ualberta.ca>
2024-11-19 01:26:32 -0700

    added percent encode function

--- backend/azureDSN/utils/url_parser.py
+++ backend/azureDSN/utils/url_parser.py
@@ -1,7 +1,10 @@
-from urllib.parse import urljoin
+from urllib.parse import urljoin, quote
 
 def get_base_host(url):
     return urljoin(url, '/').rstrip('/')
 
 def extract_uuid(url):
     return url.rstrip('/').split('/')[-1]
+
+def percent_encode(url):
+    return quote(url, safe='')
