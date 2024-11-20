b58c7e57656aeca4608c61e59fbfe6b40adc98df
erquette <erquette@ualberta.ca>
2024-11-19 22:41:10 -0700

    don't show notif from unauth remote nodes

--- backend/azureDSN/views/inbox.py
+++ backend/azureDSN/views/inbox.py
@@ -11,7 +11,7 @@ import requests, os, json
 from ..serializers import *
 from ..models import *
 from datetime import datetime
-from ..utils import *
+from ..utils import url_parser
 
 '''
 a POST request occurs if someone like, comment, share post or send follow request to our local user
@@ -103,13 +103,40 @@ class InboxView(APIView):
         
         # Get the latest inbox items
         inbox_items_obj =  InboxItem.objects.filter(inbox=inbox_obj).order_by("-time")
+
+
         
         serializer = InboxItemSerializer(inbox_items_obj, many=True, context={"request": request})
+        print(serializer.data)
+        # lists of json obj, if type is post comment or like check id
+        # try to call endpoint of id, if 200 then keep, otherwise remove the whole json from the list
+        # if follow req..?
+        filtered_data = []
+        for json in serializer.data:
+            if json.get("type") in ["like", "post", "comment"]:
+                base_host = url_parser.get_base_host(json.get('id'))
+                try:
+                    req = requests.get(
+                        f"{base_host}/api/authors/?page=1&size=1", # Any endpoint to ensure connection
+                        auth=HTTPBasicAuth(os.getenv('NODE_USERNAME'), os.getenv('NODE_PASSWORD')),
+                    )
+
+                    if req.status_code == 200:
+                        filtered_data.append(json)
+                    elif req.status_code == 403:
+                        # Local node in remote node's list, but connection not allowed
+                        continue
+                    else:
+                        continue
+                except requests.exceptions.RequestException as e:
+                    print(f"Error occurred while fetching {base_host}: {e}")
+                    return Response({"error: Something went wrong", 500})
+
         uri = request.build_absolute_uri("/")
 
         data = {
                 'user': f"{uri}api/authors/{author_serial}",
-                'items': serializer.data,
+                'items': filtered_data,
                 'type': 'inbox',
         }
         return Response(data, status=status.HTTP_200_OK)
