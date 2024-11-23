8a99ef73c23c3584b56f36efef16d6718bcb7d72
erquette <erquette@ualberta.ca>
2024-11-23 14:29:47 -0700

    use displayname instead

--- frontend/src/components/ListItem/ListItem.tsx
+++ frontend/src/components/ListItem/ListItem.tsx
@@ -207,10 +207,10 @@ export default function ListItem({
           </div>
           <div className={styles.text}>
             <h1>
-              {user.username ?? extractHost(user.host)}
+              {user.displayName ?? extractHost(user.host)}
               <span className={styles.additionalText}>{additionalText}</span>
             </h1>
-            <p>@{user.username ?? extractHost(user.host)}</p>
+            <p>@{user.displayName ?? extractHost(user.host)}</p>
           </div>
         </div>
 
