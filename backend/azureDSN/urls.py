e453b317cee6984a7b52b1c0c2e4aec54e262e25
natewu <36091727+natewu@users.noreply.github.com>
2024-11-23 21:25:59 -0700

    slash consistency

--- backend/azureDSN/urls.py
+++ backend/azureDSN/urls.py
@@ -8,7 +8,7 @@ from django.views.generic import TemplateView
 # this routes traffic from polls/ to the index function that we defined earlier in the views file.
 urlpatterns = [
     path('api/stream/', PublicStreamView.as_view(), name='stream'),
-    path('api/stream/auth', AuthStreamView.as_view(), name='auth_stream'),
+    path('api/stream/auth/', AuthStreamView.as_view(), name='auth_stream'),
     
     # Follow API
     path('api/authors/<uuid:user_id>/followers/<path:follower_url>/', FollowView.as_view(), name='followers_handler'),  
@@ -20,12 +20,12 @@ urlpatterns = [
 
     # Remote API
     path("api/authors/recommended/", RemoteAuthorsView.as_view(), name="get_recommended_authors"),
-    path("api/check/<uuid:local_serial>/follows/<path:remote_fqid>", RemoteFolloweeView.as_view(), name="check_following_status"),
+    path("api/check/<uuid:local_serial>/follows/<path:remote_fqid>/", RemoteFolloweeView.as_view(), name="check_following_status"),
 
     # Likes on Posts or Comments
     path("api/authors/<uuid:author_serial>/posts/<uuid:post_serial>/likes/", LikesView.as_view(), name="get_likes_by_serial"),
     path("api/posts/<path:post_fqid>/likes/", LikesView.as_view(), name="get_likes_by_fqid"),
-    path("api/authors/<uuid:author_serial>/posts/<uuid:post_serial>/comments/<path:comment_fqid>/likes", LikesView.as_view(), name="get_comment_likes"),
+    path("api/authors/<uuid:author_serial>/posts/<uuid:post_serial>/comments/<path:comment_fqid>/likes/", LikesView.as_view(), name="get_comment_likes"),
     
     # Comments API
     path('api/authors/<uuid:author_serial>/posts/<uuid:post_serial>/comments/',MultipleCommentsView.as_view(),name='comments_by_serial'),
