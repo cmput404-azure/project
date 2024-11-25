bf6ee6e1e3939e312697a09aacfffe9cb43760cb
erquette <erquette@ualberta.ca>
2024-11-24 22:00:39 -0700

    remote like state fix

--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -149,19 +149,19 @@ export default function Post({
 
           if (authProvider.user && postGiven.likes?.count > 0) {
             console.log(`POST GIVEN: ${JSON.stringify(postGiven.likes, null, 2)}`)
-            // setHasLiked(
-            //   postGiven.likes.src.some((like) =>
-            //     like.id.includes(authProvider.user.uuid)
-            //   )
-            // );
             setHasLiked(
-              postGiven.likes.src.some((like) => {
-                const doesInclude = like.id.includes(authProvider.user.uuid);
-                console.log(`Checking like ID: ${like.id}`);
-                console.log(`Does it include UUID (${authProvider.user.uuid})? ${doesInclude}`);
-                return doesInclude;
-              })
+              postGiven.likes.src.some((like) =>
+                like.author.id.includes(authProvider.user.uuid) // whitesmoke changed the like.id so need to compare with author.id instead
+              )
             );
+            // setHasLiked(
+            //   postGiven.likes.src.some((like) => {
+            //     const doesInclude = like.id.includes(authProvider.user.uuid);
+            //     console.log(`Checking like ID: ${like.id}`);
+            //     console.log(`Does it include UUID (${authProvider.user.uuid})? ${doesInclude}`);
+            //     return doesInclude;
+            //   })
+            // );
 
             const isShared = await ShareService.checkShare(
               postGiven.id,
