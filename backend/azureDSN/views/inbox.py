1c20a6ac3b86f3b4bc7604d3f0b63bde910d7ec3
erquette <erquette@ualberta.ca>
2024-11-24 20:50:58 -0700

    fixed local like issue wrong author

--- backend/azureDSN/views/inbox.py
+++ backend/azureDSN/views/inbox.py
@@ -968,8 +968,10 @@ class InboxView(APIView):
 
         from_remote = 'authorId' not in payload
         if not from_remote:
-            author = UserSerializer(user_object).data
-            payload['author'] = author
+            if (request and request.user):
+                author = User.objects.get(uuid=request.user.uuid)
+                payload["author"] = UserSerializer(author).data
+
             del payload['authorId']
 
         time = payload.get('published', None)
