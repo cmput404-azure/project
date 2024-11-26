7972cd1f95a924e6835d0f93396ee1c62ba07327
erquette <erquette@ualberta.ca>
2024-11-25 22:35:10 -0700

    remove unused stuff

--- frontend/src/components/NotificationList/NotificationList.tsx
+++ frontend/src/components/NotificationList/NotificationList.tsx
@@ -1,14 +1,11 @@
 // @ts-nocheck
+
 import { CircularProgress, responsiveFontSizes } from "@mui/material";
 import React, { useCallback, useEffect, useState } from "react";
-
 import ListItem from "../ListItem/ListItem";
 import Modal from "react-modal";
-import PostService from "../../service/post"
 import { api } from "../../service/config";
-import { extractUUID } from '../../util/formatting/extractUUID';
 import inbox from "../../service/inbox";
-import { normalizeURL } from '../../util/formatting/normalizeURL';
 import styles from "./NotificationList.module.scss";
 import { useAuth } from "../../state";
 
@@ -57,7 +54,6 @@ export default function NotificationList() {
               return null
             }
           } else if (item.type === "comment") {
-            let encodedId = encodeURIComponent(item.author.id);
             user = item["author"]
             try {
               let post_resp = await api.get(item.post);
