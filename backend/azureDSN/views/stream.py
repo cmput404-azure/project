7573862e4f17b30e7a5b0d4af15f73db2b858bbb
erquette <erquette@ualberta.ca>
2024-11-29 14:04:44 -0700

    fixed typo

--- backend/azureDSN/views/stream.py
+++ backend/azureDSN/views/stream.py
@@ -209,36 +209,6 @@ class AuthStreamView(APIView):
                             continue
 
                         base_author_host = url_parser.get_base_host(remote_payload.get("author").get("host"))
-                        
-
-                        # if visibility == "FRIENDS":
-                        #     # Cannot fetch whitesmoke's friends-only post with endpoint :)
-                        #     # Need to fetch likes and comments from their endpoint to make sure it's the latest
-                        #     author_serial = url_parser.extract_uuid(remote_payload.get("author").get("id"))
-                        #     post_serial = url_parser.extract_uuid(remote_payload.get("id"))
-                        #     # ://service/api/authors/{AUTHOR_SERIAL}/posts/{POST_SERIAL}/comments
-                        #     # ://service/api/posts/{POST_FQID}/comments
-                        #     # ://service/api/authors/{AUTHOR_SERIAL}/posts/{POST_SERIAL}/likes
-                        #     try:
-                        #         response = requests.get(
-                        #             f"{base_author_host}/api/authors/{author_serial}/posts/{post_serial}/likes",
-                        #             auth=HTTPBasicAuth(os.getenv('NODE_USERNAME'), os.getenv('NODE_PASSWORD'))
-                        #         )
-
-                        #         if response.json == 200:
-
-
-                        #     except Exception as e:
-                        #         print(f"Error fetching likes and comments {post_id}: {e}")
-                        #     if post_id not in remote_posts:
-                        #         remote_posts[post_id] = remote_payload
-                        #     elif item.post_status and item.post_status.upper() == "UPDATE":
-                        #         # There's a newer version of this post
-                        #         remote_posts[post_id] = remote_payload
-                        #     processed_posts[post_id] = item.post_status
-                        #     continue
-
-                        
                         encoded_post_fqid = url_parser.percent_encode(post_id)
                         get_post_url = f"{base_author_host}/api/posts/{encoded_post_fqid}"
 
@@ -271,7 +241,7 @@ class AuthStreamView(APIView):
                                             auth=HTTPBasicAuth(os.getenv('NODE_USERNAME'), os.getenv('NODE_PASSWORD'))
                                         )
 
-                                        if response.json == 200:
+                                        if response.status_code == 200:
                                             remote_payload['likes'] = response.json()
 
                                             # Fetch friends-only comments
@@ -280,7 +250,7 @@ class AuthStreamView(APIView):
                                                 auth=HTTPBasicAuth(os.getenv('NODE_USERNAME'), os.getenv('NODE_PASSWORD'))
                                             )
 
-                                            if response.json == 200:
+                                            if response.status_code == 200:
                                                 remote_payload['comments'] = response.json()
 
                                         else:
