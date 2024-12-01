326c77d52c695b0d6e70c99ce809c14575fd8ece
Quin Nguyen <hnguyen6@ualberta.ca>
2024-12-01 14:49:10 -0700

    clean code

--- backend/azureDSN/views/follow.py
+++ backend/azureDSN/views/follow.py
@@ -234,7 +234,6 @@ class FollowerView(APIView):
                 if response.status_code == 200:
                     # Now, the idea is that the folowers returned by whitesmoke is paginated and we don't need that
                     response_data = response.json()
-                    print(f"RECEVIED FOLLOWERS ARE:{response_data}")
                     if 'next' in response_data:
                         # Return 'results' if present, else return a followers format response
                         results = response_data.get('results')
