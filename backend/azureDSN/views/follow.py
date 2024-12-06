14942ee8aae3e3f4f3fab422b67857856a97ba32
xirui1219 <crystal7@ualberta.ca>
2024-12-05 22:17:32 -0700

    onesided unfollow works now

--- backend/azureDSN/views/follow.py
+++ backend/azureDSN/views/follow.py
@@ -362,11 +362,8 @@ class FollowView(APIView):
         Deletes the follow entry so that user is no longer following follower
         """
         decoded_url = unquote(follower_url)
-        parts = decoded_url.strip("/").split("/")
-        follower_id = parts[-1]
+        follower_id = url_parser.extract_uuid(decoded_url)
         # remote follower
-        print(user_id)
-        print(decoded_url)
         follower = Follow.objects.filter(local_follower_id = follower_id, remote_followee__contains=user_id)
 
         # local follower
