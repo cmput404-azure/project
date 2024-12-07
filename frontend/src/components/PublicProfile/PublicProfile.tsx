fdb6e76d058ef4b1015025b505c685e444c2351b
erquette <erquette@ualberta.ca>
2024-12-06 22:17:44 -0700

    add some note

--- frontend/src/components/PublicProfile/PublicProfile.tsx
+++ frontend/src/components/PublicProfile/PublicProfile.tsx
@@ -84,6 +84,7 @@ export default function PublicProfile() {
     const host = author?.id.split("authors")[0]
     const { count, src } = await profileService.fetchAuthorPosts(id, page, 10, host);
     setPostCount(count); // if filter is done properly, count should represent the number of public posts
+    // note: mistyrose counts deleted post as well
 
     setPosts((prevPosts) => {
       const existingIds = new Set(prevPosts.map((post) => post.id));
